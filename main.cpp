705294ad83e37646b298bdefd0eb11676b97c140
Ej19748 <ej19748@email.vccs.edu>
2024-10-24 23:47:23 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+  float starting_num_of_organisms, average_daily_population_increase, size_of_daily_population;
+  int num_of_days_to_multiply;
+  cout << "Enter the starting number of organisms: ";
+  cin >> starting_num_of_organisms;
+  while (starting_num_of_organisms < 2
+    {
+      cout << "The starting number of       organisms must be at least 2." <<      endl;
+      cout << "Enter the starting           number of organisms: ";
+      cin >> starting_num_of_organisms;
+    }  
+}
