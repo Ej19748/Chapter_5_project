1a625bda9468f7d9804cc17605bf4a3bbd342df2
Ej19748 <ej19748@email.vccs.edu>
2024-10-25 00:04:47 +0000

    Code

--- .ccls-cache/@home@runner@Chapter-5-project/main.cpp
+++ .ccls-cache/@home@runner@Chapter-5-project/main.cpp
@@ -2,38 +2,26 @@
 using namespace std;
 int main() 
 {
-  float starting_num_of_organisms, average_daily_population_increase = 0.0;
-  int num_of_days_to_multiply;
-  cout << "Enter the starting number of organisms: ";
-  cin >> starting_num_of_organisms;
-  while (starting_num_of_organisms < 2)
+  string student, first, last;
+  int numStudents;
+  cout << "Enter the number of students: ";
+  cin >> numStudents;
+  cout << "Enter the name of student 1: ";
+  cin >> student;
+  first = last = student;
+  for (int i = 1; i < numStudents; i++)
     {
-      cout << "The starting number of       organisms must be at least 2." <<      endl;
-      cout << "Enter the starting           number of organisms: ";
-      cin >> starting_num_of_organisms;
-    }
-  cout << "Enter the average daily population increase (as a percentage): ";
-  cin >> average_daily_population_increase;
-  while (average_daily_population_increase < 0)
-    {
-      cout << "The average daily population increase must be a positive value." << endl;
-      cout << "Enter the average daily population increase (as a percentage): ";
-      cin >> average_daily_population_increase;
+      cout << "Enter the name of student " << (i + 1) << ": ";
+      cin >> student;
+      if (student < first)
+        first = student;
+      if (student > last)
+        last = student;
+      {
+        cout << "First student in line = " << first << endl;
+        cout << "Last student in line = " << last << endl;
+      return 0;  
+      }
       
-    }
-  cout << "Enter the number of days they will multiply: ";
-  cin >> num_of_days_to_multiply;
-  while (num_of_days_to_multiply < 1)
-    {
-      cout << "The number of days must be at least 1." << endl; 
-      cout << "Enter the number of days they will multiply: ";
-      cin >> num_of_days_to_multiply;
-    }
-  for (int i = 0; i < num_of_days_to_multiply; i++)
-    {
-      cout << "Size of the population for day " << (i + 1) << ": " << starting_num_of_organisms << endl;
-      starting_num_of_organisms += (starting_num_of_organisms * average_daily_population_increase) / 100;
-        
-        
     }
 }
