6267e99de7e65ce42664176d9c91778e08a59474
Ej19748 <ej19748@email.vccs.edu>
2024-10-25 00:11:09 +0000

    Code

--- .ccls-cache/@home@runner@Chapter-5-project/main.cpp
+++ .ccls-cache/@home@runner@Chapter-5-project/main.cpp
@@ -1,27 +1,21 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 int main() 
 {
-  string student, first, last;
-  int numStudents;
-  cout << "Enter the number of students: ";
-  cin >> numStudents;
-  cout << "Enter the name of student 1: ";
-  cin >> student;
-  first = last = student;
-  for (int i = 1; i < numStudents; i++)
+  ifstream input_file;
+  string years;
+  cout << "population growth" << endl;
+  cout << "-----------------" << endl;
+  input_file.open ("People.txt");
+  if (input_file)
+  {
+    while (getline(input_file, years))
     {
-      cout << "Enter the name of student " << (i + 1) << ": ";
-      cin >> student;
-      if (student < first)
-        first = student;
-      if (student > last)
-        last = student;
-      {
-        cout << "First student in line = " << first << endl;
-        cout << "Last student in line = " << last << endl;
-      return 0;  
-      }
-      
+      cout << years << endl;
     }
+    input_file.close();
+    return 0;
+  }
 }
